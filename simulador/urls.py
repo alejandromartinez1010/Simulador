33bbbb814c6aa2e6657b6b6460b800d2480d27bb
alejandromartinez1010 <alea.martinez@alumnos.duoc.cl>
2019-11-20 17:51:11 -0300

    Simulador 1.2

---
+++ simulador/urls.py
@@ -0,0 +1,18 @@
+from django.urls import path 
+from . import views 
+ 
+urlpatterns = [ 
+    path('',views.index,name='index'),
+    path('consulta/',views.ListarConsultaVista.as_view(),name='consulta'),
+    path('consulta/<int:pk>',views.ListarConsultaVista.as_view(),name='consulta-detail'),
+    path('prueba/',views.prueba,name='prueba'),
+    path('contacto/',views.contacto,name='contacto'),
+] 
+
+
+urlpatterns += [ 
+    path('consulta/create',views.ConsultaCreate.as_view(),name='consulta_create'),
+    path('consulta/<int:pk>/update',views.ConsultaUpdate.as_view(),name='consulta_update'),
+    path('consulta/<int:pk>/delete',views.ConsultaDelete.as_view(),name='consulta_delete'),
+  
+] 
